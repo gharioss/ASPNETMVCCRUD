5f6929281dd33a5b0b994c5ccd9bad90be683cd0
gharioss <njulie818@gmail.com>
2022-09-20 20:28:44 +0200

    initial commit

---
+++ ASPNETMVCCRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMVCCRUD.Models.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions<MVCDemoDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
